7c011d5ec8d06471822070df1baa6362b00e689b
carltondsouza <carltondsouza@me4324-03.labs.sce.carleton.ca>
2014-09-18 16:42:09 -0400

    BuddyInfo

---
+++ BuddyInfo.java
@@ -0,0 +1,49 @@
+
+public class BuddyInfo {
+
+	private String name;
+	private String address;
+	private String phonenumber;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BuddyInfo b = new BuddyInfo();
+		System.out.println("Hello World!");
+		
+		b.setName("Alicia");
+		b.setAddress("22 Acacia Avenue");
+		b.setPhonenumber("1-810-621-9254");
+		
+		System.out.println(b.getName());
+		System.out.println(b.getAddress());
+		System.out.println(b.getPhonenumber());
+	}
+
+}
